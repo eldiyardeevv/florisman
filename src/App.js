7c1bfc219acfd83f1b5db05fa6fd265f962c57b1
eldiyardeevv <143395354+eldiyardeevv@users.noreply.github.com>
2023-11-21 16:39:22 +0600

    Shop header (#2)
    * ozgorttum
    
    * koshtum

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
-
+import Header from "./components/Navbar/Header/Header";
+import MyRoutes from "./routes/MyRoutes";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <MyRoutes />
+    </div>
+  );
 }
 
 export default App;
