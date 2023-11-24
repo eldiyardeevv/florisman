6f1bfe6f63a05a5500abd734db2d018fde103e04
eldiyardeevv <143395354+eldiyardeevv@users.noreply.github.com>
2023-11-24 18:44:41 +0600

    Otobrazhenia (#14)
    * ozgorttum
    
    * header mn footer koshtum
    
    * отображения и удаленя

--- src/components/Home/HomePage/HomePage.jsx
+++ src/components/Home/HomePage/HomePage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Home from "../Home";
 import Katalog from "../Katalog/Katalog";
-import BecentCh from "../BecentCh/BecentCh";
 import About from "../Aboutt/About";
+import ListProduct from "../../product/ListProduct";
 
 const HomePage = () => {
   return (
     <div>
       <Home />
       <Katalog />
-      <BecentCh />
+      <ListProduct />
       <About />
     </div>
   );
